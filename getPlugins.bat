908b3065c131ea351fa8c07b01c1f9491f75b797
Wuzi <wuzi@obiscr.com>
2024-05-12 17:05:37 +0800

    update android submodule git remote

--- getPlugins.bat
+++ getPlugins.bat
@@ -2,7 +2,7 @@
 
 :ParseArgs
 if "%~1" == "--shallow" (
-    call git clone git://git.jetbrains.org/idea/android.git android --depth 1
+    call git clone https://github.com/JetBrains/android.git android --depth 1
 ) else (
-    call git clone git://git.jetbrains.org/idea/android.git android
+    call git clone https://github.com/JetBrains/android.git android
 )
--- getPlugins.sh
+++ getPlugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "--shallow" ]; then
-    git clone git://git.jetbrains.org/idea/android.git android --depth 1
+    git clone https://github.com/JetBrains/android.git android --depth 1
 else
-    git clone git://git.jetbrains.org/idea/android.git android
+    git clone https://github.com/JetBrains/android.git android
 fi
